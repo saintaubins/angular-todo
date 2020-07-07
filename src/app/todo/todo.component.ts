886908874fc87cafed423c91305efe7d374ae80e
saintaubins <saintaubins@gmail.com>
2020-07-07 16:02:30 -0400

    crud is done

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from '../service/data/todo-data.service';
 import { Todo } from '../list-todos/list-todos.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -15,16 +15,42 @@ export class TodoComponent implements OnInit {
 
   constructor(
     private todoService: TodoDataService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
+
     this.id = this.route.snapshot.params['id'];
-    this.todo = new Todo(1, '', false, new Date());
-    this.todoService.retrieveTodo('in28minuets', this.id)
-    .subscribe(
-      data => this.todo = data 
-    )
+
+    this.todo = new Todo(this.id, '', false, new Date());
+
+    if(this.id!=-1) {
+      this.todoService.retrieveTodo('in28minuets', this.id)
+      .subscribe(
+        data => this.todo = data 
+      )
+    }
+  }
+
+  saveTodo() {
+    if(this.id === -1) {
+      this.todoService.createTodo('in28minuets', this.todo)
+      .subscribe (
+        data => {
+          console.log(data)
+          this.router.navigate(['todos'])
+        }
+      )
+    } else {
+      this.todoService.updateTodo('in28minuets', this.id, this.todo)
+      .subscribe (
+        data => {
+          console.log(data)
+          this.router.navigate(['todos'])
+        }
+      )
+    }
   }
 
 }
