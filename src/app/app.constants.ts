eab007a2763f4acda73eaf6f6cb2540edd467142
saintaubins <saintaubins@gmail.com>
2020-07-14 16:02:06 -0400

    testing new url

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,3 @@
-export const TODO_JPA_API_URL = "http://localhost:8080/jpa"
+export const TODO_JPA_API_URL = "https://heroku-angular-springboot.herokuapp.com/jpa"
+export const TEST_TODO_JPA_API_URL = "http://localhost:8080/jpa"
 export const API_URL = "http://localhost:8080"
